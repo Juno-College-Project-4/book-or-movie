79db0553628ab805ebb0735c0c7b34fd892fabac
Terri <terriyiu@gmail.com>
2023-02-04 15:58:07 -0500

    found all info for books api

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,37 +3,43 @@ import SearchBar from "./SearchBar";
 
 const MainComponent = () => {
   // const onEnter = () => {
-  //   axios({
-  //     url: "https://www.googleapis.com/books/v1/volumes",
-  //     method:'GET',
-  //     dataResponse: 'json',
-  //     params: {
-  //         API_KEY:"AIzaSyAUtAytZDfo4ohe1l2O29rUzdY7oypqRts",
-  //         q: "lord of the rings"
-  //     }
-  //   })
-  //   .then((data) => {
-  //       console.log(data)
-  //   })
-  //   .catch((error) => {
-  //       console.log(error)
-  //   });
-  // }
-
-  axios({
-    url: "https://api.themoviedb.org/3/discover/movie",
-    params: {
-      api_key: "abb23cb27cf45e3859e3f8c484c9463a",
-      include_adult: false,
-      primary_release_year: 2002,
-    },
-  })
+    axios({
+      url: "https://www.googleapis.com/books/v1/volumes",
+      method:'GET',
+      dataResponse: 'json',
+      params: {
+          API_KEY:"AIzaSyAUtAytZDfo4ohe1l2O29rUzdY7oypqRts",
+          q: "lord of the rings"
+      }
+    })
     .then((data) => {
-      console.log(data);
+        // console.log(data.data.items[1].volumeInfo.imageLinks.thumbnail) -> FOR IMAGE
+      // console.log(data.data.items[1].volumeInfo.description) -> FOR DESCRIPTION
+      // console.log(data.data.items[1].volumeInfo.title) -> FOR TITLE
+      // console.log(data.data.items[1].volumeInfo.authors) -> FOR AUTHORS
+      // console.log(data.data.items[1].volumeInfo.averageRating) -> FOR AVERAGE RATING
+
+      console.log(data.data.items[1].volumeInfo)
     })
     .catch((error) => {
-      console.log(error);
+        console.log(error)
     });
+  // }
+
+  // axios({
+  //   url: "https://api.themoviedb.org/3/discover/movie",
+  //   params: {
+  //     api_key: "abb23cb27cf45e3859e3f8c484c9463a",
+  //     include_adult: false,
+  //     primary_release_year: 2002,
+  //   },
+  // })
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
   // index 0 OR randomizer as each search has different
   // for google book api we need to get
---
+++ src/components/RatingsResult.js
@@ -0,0 +1,49 @@
+const RatingsResults = () => {
+    return (
+        <>
+            <section className="wrapper">
+                <div className="bookContainer">
+                    <div className="bookInfo">
+                        <div className="bookImageContainer">
+                            <img className="bookImage" src={} alt={} />
+                        </div>
+                        <div className="bookRating">
+                            
+                        </div>
+                        <div className="bookAuthor">
+
+                        </div>
+                        <div className="bookStars">
+
+                        </div>
+                        <div className="bookDescription">
+        
+                        </div>
+                    </div>
+                </div>
+
+                <div className="movieContainer">
+                    <div className="movieInfo">
+                        <div className="movieImageContainer">
+                            <img className="movieImage" src={ } alt={ } />
+                        </div>
+                        <div className="movieRating">
+
+                        </div>
+                        <div className="movieAuthor">
+
+                        </div>
+                        <div className="movieStars">
+
+                        </div>
+                        <div className="movieDescription">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default RatingsResults;
