20cdaad42846905563d48783ba8458e0a3ffd215
Jie eun Lee <jieeunlee91@gmail.com>
2023-02-09 22:03:35 -0500

    created bookToMovie function to call movie API after book API, compared book title to match movie title, and added error handling

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -13,11 +13,10 @@ const MainContainer = () => {
   const [searchInput, setSearchInput] = useState(""); 
   const [bookDetails, setBookDetails] = useState([]);
   const [movieDetails, setMovieDetails] = useState([]);
-
+  
   //4. define a side effect which will run once the user searches for a book or movie title
   const onEnter = () => {
     // console.log("this is run")
-    // console.log(bookTitle);
     axios({
       url: "https://www.googleapis.com/books/v1/volumes",
       method:'GET',
@@ -33,83 +32,59 @@ const MainContainer = () => {
       // console.log(data.data.items[1].volumeInfo.title) -> FOR TITLE
       // console.log(data.data.items[1].volumeInfo.authors) -> FOR AUTHORS
       // console.log(data.data.items[1].volumeInfo.averageRating) -> FOR AVERAGE RATING
-      const movie = apiData.data.items[1].volumeInfo
-      // const filteredBookData = apiData.data.items[1].volumeInfo.filter(obj => obj.title === bookTitle) -- trying to have a strict filter based on user input
+      const bookName = apiData.data.items[1].volumeInfo.title
+      // trying to have a strict filter based on user input
+      // const filteredBookData = apiData.data.items[1].volumeInfo.filter(obj => obj.title === bookTitle) 
       // setBookTitle(filteredBookData);
       // console.log(movie)
-      setBookDetails(apiData.data.items[1].volumeInfo);
-
+      setBookDetails(apiData.data.items[1].volumeInfo.title);
+      const bookToMovie = (bookTitle) => { 
+        console.log(bookTitle)          
+        return axios({
+            url: `https://api.themoviedb.org/3/search/movie`,
+            params: {
+              api_key: "abb23cb27cf45e3859e3f8c484c9463a",
+              include_adult: false,
+              language: "en",        
+              query: bookTitle
+          }
+        })        
+      }      
       // function goes here for bookDetails.title
       // wrap the movie axios into a function here.
-
-      
-
-
-
-      
       // console.log(setBookTitle);
-      // console.log(filteredBookData);
-    })
-    .catch((error) => {
-        console.log(error) 
-    });
-  
-    axios({
-      url: `https://api.themoviedb.org/3/search/movie`,
-      params: {
-        api_key: "abb23cb27cf45e3859e3f8c484c9463a",
-        include_adult: false,
-        language: "en",
-        
-        query: searchInput
-      },
-    })
-  
-      .then((data) => {
-         
-        // filtered movie data based on user input 
-        
-        const filteredMovieData = data.data.results.filter(movie => movie.original_title.toUpperCase() === searchInput.toUpperCase() )
-        const popular = filteredMovieData.map(popularMovie => console.log(popularMovie))
-        
-        
-        
-
-
-
-        const popularOrganized = (Math.max(...popular))
-        
+      // console.log(filteredBookData);   
+      bookToMovie(bookName).then((data) => {               
+        // filtered movie data based on user input         
+        const filteredMovieData = data.data.results.filter(movie => movie.original_title.toUpperCase() === bookName.toUpperCase())
+        const popular = filteredMovieData.map(popularMovie => popularMovie.popularity)  
+        const popularOrganized = (Math.max(...popular))        
         const mostPopular = popular.indexOf(popularOrganized)
         console.log(mostPopular, "most popular");
-
         // console.log(searchInput);
         // console.log(data.data.results)
         setMovieDetails(filteredMovieData[mostPopular]);
-        console.log(movieDetails, "movie Details");
-      })
-  
-      .catch((error) => {
-        console.log(error);
-      });
+        // console.log(movieDetails, "movie Details");
+      }) 
+    })      
+    .catch((error) => {
+       alert("Invalid book title!")
+    });
   }
-
+  
   // index 0 OR randomizer as each search has different
   // for google book api we need to get
   // index1, volumeInfo -> averageRating, volumeInfo ->description, volumeInfo -> imageLinks -> thumbnail, volumneInfo -> authors,
-const handleClick = (e) => {
-  e.preventDefault();
+  const handleClick = (e) => {
+    e.preventDefault();
 
-  // call the state updater function and use the selected option value to update.
-  onEnter();
+    // call the state updater function and use the selected option value to update.
+    onEnter();
 
-  // setBookDetails(displayDetails);
+    // setBookDetails(displayDetails);
 
-  // setMovieDetails(displayDetails);
-
-
-
-}
-
+    // setMovieDetails(displayDetails);
+  }
   return (
     <section>
       {/*Parent component that will be used to pass down props */}
