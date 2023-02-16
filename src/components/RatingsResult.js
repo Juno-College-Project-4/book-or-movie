9af20a595afa208a805f36d7d8dd194290607896
Dave Hughes <hughesd11@gmail.com>
2023-02-15 21:16:54 -0500

    on click completed to grab book title from Ratings Result component and pass it to the Movie API - This works but we would like to revisit the Movie API call to ensure the best and most forgiving way to search

--- src/components/RatingsResult.js
+++ src/components/RatingsResult.js
@@ -1,36 +1,36 @@
 const RatingsResults = (props) => {
-    const searchInput = props.searchInput
-    const bookInfo = props.bookDetails
-    console.log(bookInfo)
-    const error = props.error
-    return (
-        <>
-            <section className="wrapper">
-                <div className="bookContainer">
-                    <div className="bookInfo">
-                        <h2>{searchInput}</h2>
-                        <p>
-                            {
-                                error ? error : ""
-                            }
-                            {/* stretch goal: hide the paragraph vs. what's inside the paragraph */}
-                        </p>
-                        <div className="bookImageContainer">
-                            {bookInfo.map((bookDescription, i) => {
-                                return (
-                                    <div key={i}>
-                                        <img src={bookDescription.imageLinks.thumbnail} alt="" />
-                                        <h3 key={i}>{bookDescription.title}</h3>
-                                    </div>
-                                    
-                                )  
-                            })}
-                        </div>
-                </div>  
+  const searchInput = props.searchInput;
+  const bookInfo = props.bookDetails;
+  console.log(bookInfo);
+  const error = props.error;
+  return (
+    <>
+      <section className="wrapper">
+        <div className="bookContainer">
+          <div className="bookInfo">
+            <h2>{searchInput}</h2>
+            <p>
+              {error ? error : ""}
+              {/* stretch goal: hide the paragraph vs. what's inside the paragraph */}
+            </p>
+            <div className="bookImageContainer">
+              {bookInfo.map((bookDescription, i) => {
+                return (
+                  <div
+                    onClick={() => props.onClick(bookDescription.title)}
+                    key={i}
+                  >
+                    {/* <img src={bookDescription.imageLinks.thumbnail} alt="" /> */}    
+                    <h3>{bookDescription.title}</h3>
+                  </div>
+                );
+              })}
             </div>
-            </section>
-        </>
-    )
-}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default RatingsResults;
